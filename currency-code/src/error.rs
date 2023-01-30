14afff91574e47a56066c766779db5b8ec9ec17b
vkill <vkill.net@gmail.com>
2023-01-30 21:27:47 +0800

    Tweak

--- currency-code/src/error.rs
+++ currency-code/src/error.rs
@@ -1,13 +1,11 @@
-use core::fmt;
-
 //
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ParseError {
     Invalid(::alloc::boxed::Box<str>),
 }
 
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl core::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{self:?}")
     }
 }
